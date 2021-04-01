f906aa82fd3e8a97d9cefe05c6ff33d8e8dadc29
박태정\taeju <taejung3911@naver.com>
2021-04-01 15:23:35 +0900

    study_1.c 파일생성
    정수형, 실수형, 상수형 실수를 각각 a,b,c로 선언한 다음 scanf_s를 통하여 a에 정수입력을 받아 각각의 변수를 출력한 프로그램.

---
+++ study_1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void) {
+
+	int a;
+	
+	
+	float b;
+	const float c = 3.141592;
+
+	scanf_s("%d", &a);
+	
+	printf("a = %d\n", a<<3);
+	b = 3.14;
+
+
+
+	 printf("%f\n %.3f\n", b, c);
+
+
+
+	return 0;
+}
