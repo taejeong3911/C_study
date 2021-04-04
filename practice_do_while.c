d6c258c54f623f1cd3aac47f0fe67c66a42abcda
박태정\taeju <taejung3911@naver.com>
2021-04-04 21:51:19 +0900

    Group study_C_2
    practice do while & scanf_s

---
+++ practice_do_while.c
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+int main() {
+	int i = 1;
+	int input;
+	scanf_s("%d", &input);
+	do {
+
+		int j = 1;
+		while (j < input) {
+			printf("%d * %d = %d\n", i, j, i * j);
+			j++;
+		}
+		i++;
+	}
+
+
+	while (i < input); {
+		
+		printf("\n");
+	}
+}
