b4f965f357c8980f82989a2c3a5a88b8497344b2
박태정\taeju <taejung3911@naver.com>
2021-04-28 11:15:22 +0900

    using_func_!.c

---
+++ using_f_!.c
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+
+unsigned int rec_sum(unsigned int i) {
+
+	unsigned int sum;
+
+	if (i == 0) sum = 0;
+	else sum = rec_sum(i - 1) + i;
+	return sum;
+
+
+
+}
+
+
+
+int main() {
+	
+	int num;
+	num = rec_sum(5);
+
+	printf("%d", num);
+
+}
